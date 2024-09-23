b74651054590679e23a427dcd3c5e60dd615a911
Chris-Mathias <christian.michelson@sou.unijui.edu.br>
2024-09-23 19:18:13 -0300

    v0.1.6 Modal

--- src/contexts/modalContext.jsx
+++ src/contexts/modalContext.jsx
@@ -3,7 +3,6 @@ import { createContext, useState, useContext } from "react";
 const ModalContext = createContext();
 
 export const ModalProvider = ({ children }) => {
-<<<<<<< HEAD
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const openModal = () => setIsModalOpen(true);
@@ -17,21 +16,6 @@ export const ModalProvider = ({ children }) => {
             {children}
         </ModalContext.Provider>
     );
-=======
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-  const toggleModal = () => setIsModalOpen((prev) => !prev);
-
-  return (
-    <ModalContext.Provider
-      value={{ isModalOpen, openModal, closeModal, toggleModal }}
-    >
-      {children}
-    </ModalContext.Provider>
-  );
->>>>>>> 3914c92cf9f56eea1b367b7e5706d7dd246f9b3d
 };
 
 export const useModal = () => useContext(ModalContext);
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,6 @@ import SideBarComp from "../components/sideBar";
 import GridComp from "../components/grid";
 import Modal from "../components/modal";
 import { SideBarProvider } from "@/contexts/sideBarContext";
-<<<<<<< HEAD
 import { DataProvider } from "@/contexts/dataContext";
 import { ModalProvider } from "@/contexts/modalContext";
 
@@ -27,36 +26,4 @@ export default function Home() {
             </SideBarProvider>
         </DataProvider>
     );
-=======
-import { useEffect, useState } from "react";
-import { ModalProvider } from "@/contexts/modalContext";
-
-export default function Home() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/turmas")
-      .then((response) => response.json()) // Converte a resposta para JSON
-      .then((data) => {
-        console.log(data); // Exibe o JSON no console
-        setData(data); // Define o estado com o JSON recebido
-      })
-      .catch((error) => console.error("Erro ao buscar os dados:", error));
-  }, []);
-
-  return (
-    <SideBarProvider>
-      <ModalProvider>
-        <div className="h-screen bg-base-100">
-          <NavBarComp />
-          <div className="flex" style={{ height: "calc(100vh - 65px)" }}>
-            <GridComp />
-            <SideBarComp />
-          </div>
-          <Modal />
-        </div>
-      </ModalProvider>
-    </SideBarProvider>
-  );
->>>>>>> 3914c92cf9f56eea1b367b7e5706d7dd246f9b3d
 }
